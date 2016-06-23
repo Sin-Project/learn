409f0eebc91602ced2e82442aa2c095fe4b3de45
magce.qin <magce.qin@magceqindeMac-mini.local>
2016-06-23 15:39:06 +0800

    first python add

---
+++ python/Basics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+''' python basics '''
+
+__author__ = 'magce';
+__source__ = 'Michael Liao';
+
+# list
+classmates = ['Mechael', 'Bob', 'Tracy'];
+print classmates;
+
+
