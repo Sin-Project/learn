7ad9ef0b6e60b79b398aed7a33335277f2b2ecdc
magce.qin <magce.qin@magceqindeMac-mini.local>
2016-06-24 16:52:23 +0800

    fix

--- python/Basics.py
+++ python/Basics.py
@@ -12,10 +12,10 @@ import customized.fn as fn;
 integer = 1546685;
 print dir(integer);
 ''' ['__abs__', '__add__', '__and__', '__class__', '__cmp__', '__coerce__', '__delattr__', 
-...'__div__', '__divmod__', '__doc__', '__float__', '__floordiv__', '__format__', 
-...'__getattribute__', '__getnewargs__', '__hash__', '__hex__', '__index__', '__init__', 
-...'__int__', '__invert__', '__long__', '__lshift__', '__mod__', '__mul__', '__neg__', 
-...'__new__', '__nonzero__', '__oct__', '__or__', '__pos__', '__pow__', '__radd__',
+... '__div__', '__divmod__', '__doc__', '__float__', '__floordiv__', '__format__',
+... '__getattribute__', '__getnewargs__', '__hash__', '__hex__', '__index__', '__init__',
+... '__int__', '__invert__', '__long__', '__lshift__', '__mod__', '__mul__', '__neg__',
+... '__new__', '__nonzero__', '__oct__', '__or__', '__pos__', '__pow__', '__radd__',
 ... '__rand__', '__rdiv__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__',
 ... '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__rpow__',
 ... '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setattr__',
