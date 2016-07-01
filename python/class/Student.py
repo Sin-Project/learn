6de3551abd25cc6aaeac96a79a5598b2247af660
magce.qin <magce.qin@magceqindeMac-mini.local>
2016-07-01 18:02:57 +0800

    mofied

--- python/class/Student.py
+++ python/class/Student.py
@@ -8,18 +8,18 @@ __author__ = 'magce';
 class Student(object):
 	pass;
 
-# print dir(Student);
-# ''' ['__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__',
-# ... '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
-# ... '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']'''
+print dir(Student);
+''' ['__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__',
+... '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
+... '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']'''
 
-# print dir(Student.__class__);
-# ''' ['__abstractmethods__', '__base__', '__bases__', '__basicsize__', '__call__', '__class__',
-# ... '__delattr__', '__dict__', '__dictoffset__', '__doc__', '__eq__', '__flags__', '__format__',
-# ... '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__instancecheck__',
-# ... '__itemsize__', '__le__', '__lt__', '__module__', '__mro__', '__name__', '__ne__', '__new__',
-# ... '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
-# ... '__subclasscheck__', '__subclasses__', '__subclasshook__', '__weakrefoffset__', 'mro']'''
+print dir(Student.__class__);
+''' ['__abstractmethods__', '__base__', '__bases__', '__basicsize__', '__call__', '__class__',
+... '__delattr__', '__dict__', '__dictoffset__', '__doc__', '__eq__', '__flags__', '__format__',
+... '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__instancecheck__',
+... '__itemsize__', '__le__', '__lt__', '__module__', '__mro__', '__name__', '__ne__', '__new__',
+... '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
+... '__subclasscheck__', '__subclasses__', '__subclasshook__', '__weakrefoffset__', 'mro']'''
 
 bart = Student();
 
