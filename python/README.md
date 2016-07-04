7c5b430bbf8f8ccb4aaecfc65abbf3aa28ba9693
magce.qin <magce.qin@magceqindeMac-mini.local>
2016-07-04 17:27:15 +0800

    modified

--- python/README.md
+++ python/README.md
@@ -1,4 +1,4 @@
-# python 2.7
+# Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 12:54:16) 
 	Basic and class
 		./basics.py
 		./class/BClass.py
--- python/customized/fn.py
+++ python/customized/fn.py
@@ -20,3 +20,80 @@ def readObjectAttr(obj, attr):
         return obj.attr;
     return None
 
+# try:
+#     print 'try...'
+#     r = 10 / int(2);
+#     print 'result:', r
+# except ValueError, e:
+#     print 'ValueError:', e
+# except ZeroDivisionError, e:
+#     print 'ZeroDivisionError:', e
+# else:
+#     print 'no error!'
+# finally:
+#     print 'finally...'
+# print 'END'
+#
+# 	try...
+# 	result: 5
+# 	no error!
+# 	finally...
+# 	END
+
+
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def domain():
+    try:
+        bar('0');
+    except StandardError, e:
+        print 'Error : %s!' % e;
+    finally:
+        print 'finally...'
+# domain();
+#	Error : integer division or modulo by zero!
+#	finally...
+
+
+
+def foo(s):
+    n = int(s)
+    return 10 / n
+
+def bar(s):
+    try:
+    	assert isinstance(s, int), 'type error';
+    	assert s != 0, 'zero';
+        return foo(s) * 2;
+    except StandardError, e:
+        print 'Error!'
+        raise
+
+def domain():
+    bar(0);
+# raise error     when assert = false active error
+
+
+import logging
+
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def domain():
+    try:
+        bar('0')
+    except StandardError, e:
+        logging.exception(e)
+
+domain();
+print 'END';
+# 	...
+# 	ZeroDivisionError: integer division or modulo by zero
+# 	END
