17351ceec7cd1c7eaa8820552441a56ef2e57bd3
magce.qin <magce.qin@magceqindeMac-mini.local>
2016-07-08 12:28:20 +0800

    service

--- net/sock.py
+++ net/sock.py
@@ -3,7 +3,7 @@
 
 import socket;
 
-# create connect  AF_INET : IPv4 ; 
+# create connect   AF_INET : IPv4 ; SOCK_STREAM : TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 
 s.connect(('www.sina.com.cn', 80));
